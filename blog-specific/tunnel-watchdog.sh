6372ed8969a820ca426ac3c2c39007468af7b97d
Bojie Li <bojieli@gmail.com>
2014-06-29 11:13:41 +0800

    watchdog: double check tunnel state before switching

--- blog-specific/tunnel-watchdog.sh
+++ blog-specific/tunnel-watchdog.sh
@@ -3,7 +3,10 @@
 
 cd $(dirname $0)
 
-gw=$(ip route get 8.8.8.8 | head -n 1 | awk '{print $3}')
+function getgw() {
+    ip route get 8.8.8.8 | head -n 1 | awk '{print $3}'
+}
+gw=$(getgw)
 [ -z "$gw" ] && exit 1
 
 if [ "$gw" = "10.0.2.1" ]; then
@@ -12,14 +15,18 @@ if [ "$gw" = "10.0.2.1" ]; then
     for i in {1..5}; do
         curl -4 --connect-timeout 10 --max-time 30 http://googleblog.blogspot.com/ >/dev/null 2>&1 && exit 0
     done
-    # switch off default route from tunnel
-    export IPV6_GRE_TUNNEL_ENABLE=false
-    export IPV4_GRE_TUNNEL_ENABLE=false
-    output=$(./ip-route)
-    mutt -s "VPN tunnel test failed" -- "servmon@blog.ustc.edu.cn" <<EOF
+    # in case other probes have switched off the tunnel... double check
+    if [ "$(getgw)" = "10.0.2.1" ]; then
+        # switch off default route from tunnel
+        export IPV6_GRE_TUNNEL_ENABLE=false
+        export IPV4_GRE_TUNNEL_ENABLE=false
+        output=$(./ip-route)
+        mutt -s "VPN tunnel test failed" -- "servmon@blog.ustc.edu.cn" <<EOF
 Tunnel switched off.
 Output: $output
 EOF
+        [ -x "./try-reboot-vps" ] && ./try-reboot-vps
+    fi
 else
 # tunnel is disabled
     # if succeed for all 10 trials...
@@ -27,13 +34,16 @@ else
         curl --interface do2 -4 --connect-timeout 10 --max-time 30 http://googleblog.blogspot.com/ >/dev/null 2>&1 || exit 0
         sleep 5
     done
-    # switch on default route via tunnel
-    export IPV6_GRE_TUNNEL_ENABLE=true
-    export IPV4_GRE_TUNNEL_ENABLE=true
-    output=$(./ip-route)
-    /etc/init.d/bind9 restart  ## clean DNS cache which may have been polluted
-    mutt -s "VPN tunnel recovered" -- "servmon@blog.ustc.edu.cn" <<EOF
+    # in case other probes have switched on the tunnel... double check
+    if [ "$(getgw)" != "10.0.2.1" ]; then
+        # switch on default route via tunnel
+        export IPV6_GRE_TUNNEL_ENABLE=true
+        export IPV4_GRE_TUNNEL_ENABLE=true
+        output=$(./ip-route)
+        /etc/init.d/bind9 restart  ## clean DNS cache which may have been polluted
+        mutt -s "VPN tunnel recovered" -- "servmon@blog.ustc.edu.cn" <<EOF
 Tunnel switched on.
 Output: $output
 EOF
+    fi
 fi
